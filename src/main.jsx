dca599fe250d18d1cfd00a7ef2588feaae9f89b3
naStipe <stephankhlebushkin@gmail.com>
2024-02-05 11:21:47 +0200

    Half-way done.

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root.jsx";
+import ErrorPage from "./error-page.jsx";
+import Contact from "./routes/contact.jsx";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        loader: rootLoader,
+        action: rootAction,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
